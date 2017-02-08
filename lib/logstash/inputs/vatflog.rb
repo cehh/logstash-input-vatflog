c94f88389350222be7b17c54b2b6c4a1ec1bc7f3
Carlos Hernandez <ceh@ti.com>
2017-02-08 11:05:07 -0500

    Define logstash-input-vatflog plugin
    An input plugin to generate vatf test log events.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

---
+++ lib/logstash/inputs/vatflog.rb
@@ -0,0 +1,62 @@
+# encoding: utf-8
+require "logstash/inputs/base"
+require "logstash/namespace"
+require "stud/interval"
+require "socket" # for Socket.gethostname
+
+# Generate a repeating message.
+#
+# This plugin is intented only as an example.
+
+class LogStash::Inputs::Vatflog < LogStash::Inputs::Base
+  config_name "vatflog"
+
+  # If undefined, Logstash will complain, even if codec is unused.
+  default :codec, "plain"
+
+  # The base directory where all test logs are stored
+  config :baselogdir, :validate => :uri, :required => true
+
+  # The farm setup whose logs must be process
+  config :farm, :validate => :string, :required => true
+
+  # Set how frequently messages should be sent.
+  #
+  # The default, `3600`, means send a message every hour.
+  config :interval, :validate => :number, :default => 3600
+
+  public
+  def register
+    @host = Socket.gethostname
+  end # def register
+
+  def run(queue)
+    # we can abort the loop if stop? becomes true
+    while !stop?
+      date = Time.now.strftime("%m_%d_%Y")
+      sessions = Dir.glob("#{@baselogdir}/#{@farm}/#{@farm}#{date}*/session.html").select{|f| (Time.now - File.mtime(f)) <= @interval }
+      sessions.each {|session_log|
+        iter_log =  Dir.glob("#{File.dirname(session_log)}/**/iterZummary.html")[0]
+        dut_log = Dir.glob("#{File.dirname(session_log)}/**/dut1*")[0]
+        platform_log = `cat #{session_log} | grep Platform`
+        message = "#{FIle.read(dut_log)} =@=@=@ Start of vatf log =@=@=@\n #{File.read(iter_log)} =@=@=@ Start of session log =@=@=@\n #{File.read(platform_log)}"
+        event = LogStash::Event.new("message" => message, "host" => @host)
+        decorate(event)
+        queue << event
+      }
+      # because the sleep interval can be big, when shutdown happens
+      # we want to be able to abort the sleep
+      # Stud.stoppable_sleep will frequently evaluate the given block
+      # and abort the sleep(@interval) if the return value is true
+      Stud.stoppable_sleep(@interval) { stop? }
+    end # loop
+  end # def run
+
+  def stop
+    # nothing to do in this case so it is not necessary to define stop
+    # examples of common "stop" tasks:
+    #  * close sockets (unblocking blocking reads/accepts)
+    #  * cleanup temporary files
+    #  * terminate spawned threads
+  end
+end # class LogStash::Inputs::Vatflog
